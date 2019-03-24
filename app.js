8700d2b0dd6f74d5d35965ade7ad462ba03b9a3b
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-03-23 23:17:48 -0400

    Create functional tests

--- app.js
+++ app.js
@@ -4,11 +4,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+const routes = require('./routes/api');
 const port = 4000;
 
+app.use(cors());
 app.use(express.static('public'));
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
@@ -16,3 +20,4 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
+module.exports = app;
---
+++ tests/functional-test.js
@@ -0,0 +1,79 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', () => {
+
+  suite('Routing Tests', () => {
+    
+    suite('GET /api/convert => conversion object', () => {
+      
+      test('Convert 10L (valid input)', (done) => {
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: '10L'})
+          .end( (err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 10);
+            assert.equal(res.body.initUnit, 'L');
+            assert.approximately(res.body.returnNum, 2.64172, 0.1);
+            assert.equal(res.body.returnUnit, 'gal');
+            done();
+          });
+      });
+      
+      test('Convert 32g (invalid input unit)', (done) => {
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: '32g'})
+          .end( (err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, 'Invalid unit');
+            done();
+          });
+      });
+      
+      test('Convert 3/7.2/4kg (invalid value)', (done) => {
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: '3/7.2/4kg'})
+          .end( (err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, 'Invalid value');
+            done();
+          });
+      });  
+      
+      test('Convert 3/7.2/4kilomegagram (invalid value and unit)', (done) => {
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: '3/7.2/4kilomegagram'})
+          .end( (err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.error, 'Invalid value and unit');
+            done();
+          });
+      });
+      
+      test('Convert kg (no number)', (done) => {
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: 'kg'})
+          .end( (err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 1);
+            assert.equal(res.body.initUnit, 'kg');
+            assert.approximately(res.body.returnNum, '2.2045', 0.1);
+            assert.equal(res.body.returnUnit, 'lbs');
+            done();
+          });
+      });
+    
+    });
+
+  });
+
+});
