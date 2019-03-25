6a956ffa5d053ffd1d87cd0966b57456f0148fe5
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-03-24 21:32:39 -0400

    Add ky.js and fetch data from api

--- public/main.js
+++ public/main.js
@@ -1,9 +1,29 @@
-function toggle() {
-  var div = document.getElementById("toggle-div");
+'use strict';
+import ky from '/ky.js';
 
+const inputField = document.getElementById('convertField');
+const div = document.getElementById("toggle-div");
+const result = document.getElementById("result");
+const jsonResult = document.getElementById("jsonResult");
+
+function toggle() {
   if(div.style.display == 'block') {
       div.style.display = 'none';
   } else {
     div.style.display = 'block';
   }
 }
+
+document.getElementById("convertForm").addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let APIurl = window.location.href + "api/convert?input=";
+  let input = inputField.value;
+  let urlRequest = APIurl + input;
+
+  (async () => {
+    const data = await ky.get(urlRequest).json();
+    result.textContent = data.string;
+    jsonResult.textContent = JSON.stringify(data);
+  })();
+});
